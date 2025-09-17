5fd53232e9c0ca96d4204f9c14e96925155abd21
keegang6705 <darunphobwi@gmail.com>
2025-09-17 14:12:15 +0700

    change lang detection logic

--- main.py
+++ main.py
@@ -345,13 +345,15 @@ class ArknightsOCRApp(QMainWindow):
             print(f"Auto-crop failed: {e}")
             return image_pillow
     
-    def detect_language_from_image(self, image):
+    def detect_language_from_image(image):
         try:
-            temp_text = pytesseract.image_to_string(image, config="--psm 6 -l eng").strip()
-            if not temp_text:
-                temp_text = pytesseract.image_to_string(image, config="--psm 6 -l chi_sim").strip()
+            temp_text = pytesseract.image_to_string(image, config="--psm 6 -l chi_sim+eng").strip()
             if not temp_text:
                 return "ENG"
+
+            if re.search(r'[\u4e00-\u9fff]', temp_text):
+                return "CHI"
+
             lang = detect(temp_text)
             if lang.startswith("zh"):
                 return "CHI"
